3c17ddaf6fe2a71d1201afeca9caba91a22b4e52
taiga0069 <52093403+taiga0069@users.noreply.github.com>
2020-03-16 22:44:20 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,55 @@
+$(function(){
+  $('#login-show').click(function(){
+    $('#login-modal').fadeIn();
+  });
+
+  $('.signup-show').click(function(){
+    $('#signup-modal').fadeIn();
+  });
+
+  $('.close-modal').click(function(){
+    $('#login-modal').fadeOut();
+    $('#signup-modal').fadeOut();
+  });
+
+  $('.team-hover').hover(
+    function(){
+      $(this).find('.text-contents').addClass('text-active');
+    },
+    function(){
+      $(this).find('.text-contents').removeClass('text-active');
+    }
+  );
+
+  $('.faq-list-item').click(function(){
+    var $answer = $(this).find('.answer');
+
+    if($answer.hasClass('open')){
+      $answer.removeClass('open');
+
+      $answer.slideUp();
+
+      $(this).find('span').text('+');
+
+    }else{
+      $answer.addClass('open');
+
+      $answer.slideDown();
+
+      $(this).find('span').text('-');
+    }
+  });
+
+  $('#top-btn').click(function(){
+    $('html,body').animate({'scrollTop':0},2000);
+  })
+
+  $('.header-right a').click(function(){
+    var id = $(this).attr('href');
+    var position = $(id).offset().top;
+    $('html,body').animate({
+      'scrollTop':position
+    },1500);
+  })
+
+});
